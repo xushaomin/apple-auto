158bb45cab717cba3d1807fc9e813e775b1cc4ec
徐少敏 <xushaomin@9zhitx.com>
2017-06-14 11:43:42 +0800

    初始提交

--- apple-auto-storager/apple-auto-storager-location/src/main/java/com/appleframework/auto/storager/location/service/impl/NewestLocationServiceImpl.java
+++ apple-auto-storager/apple-auto-storager-location/src/main/java/com/appleframework/auto/storager/location/service/impl/NewestLocationServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import com.appleframework.auto.storager.location.service.NewestLocationService;
@@ -13,12 +14,18 @@ import com.hazelcast.core.HazelcastInstance;
 @Service("newestLocationService")
 public class NewestLocationServiceImpl implements NewestLocationService {
 
+	protected final static Logger logger = Logger.getLogger(HbaseLocationServiceImpl.class);
+
 	@Resource
     private HazelcastInstance hazelcastInstance;
 
 	@Override
 	public void save(Location location) {
         Map<String, Location> newestLocationMap = hazelcastInstance.getMap("NEWEST_LOCATION");
+        String account = location.getAccount();
+        if(logger.isDebugEnabled()) {
+			logger.debug(account);
+		}
         newestLocationMap.put(location.getAccount(), location);
 	}
 
