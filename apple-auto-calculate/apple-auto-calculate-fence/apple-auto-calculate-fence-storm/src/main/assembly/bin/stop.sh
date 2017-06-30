921da54d6862a10ccd5ea32e3d7967d0e136ae72
徐少敏 <xushaomin@9zhitx.com>
2017-06-30 14:57:26 +0800

    优化启动脚本

--- apple-auto-calculate/apple-auto-calculate-fence/apple-auto-calculate-fence-storm/src/main/assembly/bin/stop.sh
+++ apple-auto-calculate/apple-auto-calculate-fence/apple-auto-calculate-fence-storm/src/main/assembly/bin/stop.sh
@@ -11,34 +11,27 @@ if [ -z "$SERVER_NAME" ]; then
     SERVER_NAME=`hostname`
 fi
 
-PIDS=`ps -ef | grep java | grep "$CONF_DIR" |awk '{print $2}'`
-if [ -z "$PIDS" ]; then
+SERVER_COUNT=`storm list | grep $SERVER_NAME | wc -l`
+if [ $SERVER_COUNT -lt 1 ]; then
     echo "ERROR: The $SERVER_NAME does not started!"
     exit 1
 fi
 
-if [ "$1" != "skip" ]; then
-    $BIN_DIR/dump.sh
-fi
-
 echo -e "Stopping the $SERVER_NAME ...\c"
-for PID in $PIDS ; do
-    kill $PID > /dev/null 2>&1
-done
+
+storm kill $SERVER_NAME > /dev/null 2>&1
 
 COUNT=0
 while [ $COUNT -lt 1 ]; do    
     echo -e ".\c"
     sleep 1
     COUNT=1
-    for PID in $PIDS ; do
-        PID_EXIST=`ps -f -p $PID | grep java`
-        if [ -n "$PID_EXIST" ]; then
-            COUNT=0
-            break
-        fi
-    done
+    PID_EXIST=`storm list | grep $SERVER_NAME`
+    if [ -n "$PID_EXIST" ]; then
+        COUNT=0
+        break
+    fi
 done
 
 echo "OK!"
-echo "PID: $PIDS"
+echo "Worker: $SERVER_NAME"
