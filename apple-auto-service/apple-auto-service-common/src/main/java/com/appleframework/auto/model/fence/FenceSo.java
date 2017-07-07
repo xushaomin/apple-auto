e35b022ab1de38bc4491f1a9542eb98ad9168797
徐少敏 <xushaomin@9zhitx.com>
2017-07-07 13:55:02 +0800

    新增围栏管理

---
+++ apple-auto-service/apple-auto-service-common/src/main/java/com/appleframework/auto/model/fence/FenceSo.java
@@ -0,0 +1,59 @@
+package com.appleframework.auto.model.fence;
+
+import java.io.Serializable;
+
+public class FenceSo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	private Integer fenceType;
+
+	private Boolean isEnable;
+
+	private Boolean isDelete;
+
+	private Integer dealType;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getFenceType() {
+		return fenceType;
+	}
+
+	public void setFenceType(Integer fenceType) {
+		this.fenceType = fenceType;
+	}
+
+	public Boolean getIsEnable() {
+		return isEnable;
+	}
+
+	public void setIsEnable(Boolean isEnable) {
+		this.isEnable = isEnable;
+	}
+
+	public Boolean getIsDelete() {
+		return isDelete;
+	}
+
+	public void setIsDelete(Boolean isDelete) {
+		this.isDelete = isDelete;
+	}
+
+	public Integer getDealType() {
+		return dealType;
+	}
+
+	public void setDealType(Integer dealType) {
+		this.dealType = dealType;
+	}
+
+}
